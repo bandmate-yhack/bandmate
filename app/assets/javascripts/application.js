2b405afcef793f833718d6ceabcfe5fcf4236bb6
Lee Danilek <lee.danilek@yale.edu>
2015-11-07 22:13:25 -0500

    trying for fb music again

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -129,11 +129,12 @@ function fbPermissions(callback) {
 // music and profile picture might actually require /ser-id/ instead of /me/
 function fbMusic() {
     console.log("looking for music");
-    /*FB.api('/me/music', function(response) {
+    FB.api('/me/music', function(response) {
         // look in created_time field for the time the user liked the page
+        console.log("found music");
         console.log(response);
         document.getElementById("lee").innerHTML = response;
-    });*/
+    });
 }
 
 function fbProfilePic(callback) {
