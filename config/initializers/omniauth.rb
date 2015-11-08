7cdce39ba09dcf22e8b060fac15af2532ba40b3c
Alex Reinking <alex.reinking@yale.edu>
2015-11-07 23:29:46 -0500

    adding perms to omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
+           :scope => 'email,user_likes,user_actions.music'
 end
